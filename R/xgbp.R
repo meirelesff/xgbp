178c43138f67e79805606d6536bb6108f93eec11
meirelesff <meirelesff@hotmail.com>
2021-08-16 12:18:56 -0300

    Test

--- R/xgbp.R
+++ R/xgbp.R
@@ -133,7 +133,8 @@ xgbp <- function(survey, census, census_count, ..., dep_var = NULL,
 
   # Create the output
   res <- list(estimates = res, model = mod, data = dados, nrounds = nrounds,
-              census = census, census_count = census_count, covars_matrix = est_mt,
+              census = census, #census_count = census_count,
+              covars_matrix = est_mt,
               covars = ..., dep_var = dep)
   class(res) <- c("xgbp")
 
